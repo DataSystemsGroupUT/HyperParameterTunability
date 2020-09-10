ac0c81979703a169fffe37e37ca935d6a4aa9f0b
mbahmani <mohamadjavad.bahmani@gmail.com>
2020-09-10 12:44:15 +0300

    some changes on the name of the notebooks.

--- Tools/kde_plots.py
+++ Tools/kde_plots.py
@@ -11,34 +11,34 @@ import pickle
 import scipy.stats
 import seaborn as sns
 import warnings
-from tools.helper import determine_relevant
-from tools.helper import marginal_plots
-from tools.helper import obtain_marginal_contributions
-from tools.helper import cls_kde_plot
+from Tools.helper import determine_relevant
+from Tools.helper import marginal_plots
+from Tools.helper import obtain_marginal_contributions
+from Tools.helper import cls_kde_plot
 
 
 if __name__ == "__main__":
     # Create KDE Plots
 
     #Adaboost
-    cls_kde_plot("performance_data/AB_results_total.csv","adaboost","max_depth",1,10,0.07,0.11)
+    cls_kde_plot("../performance_data/AB_results_total.csv","adaboost","max_depth",1,10,0.07,0.11)
 
     #Decision Tree
-    cls_kde_plot("performance_data/DT_results_total.csv","Decision Tree","max_features",0,1,0,3.5)
+    cls_kde_plot("../performance_data/DT_results_total.csv","Decision Tree","max_features",0,1,0,3.5)
 
     #Random Forest
-    cls_kde_plot("performance_data/RF_results_total.csv","Random Forest","min_samples_leaf",0,20,0.03,0.08,b=0.65)
+    cls_kde_plot("../performance_data/RF_results_total.csv","Random Forest","min_samples_leaf",0,20,0.03,0.08,b=0.65)
 
 
     #Extra Tree
-    cls_kde_plot("performance_data/ET_results_total.csv","Extra Tree","min_samples_leaf",1,20,0.01,0.08)
+    cls_kde_plot("../performance_data/ET_results_total.csv","Extra Tree","min_samples_leaf",1,20,0.01,0.08)
 
     # Gradient Boosting
-    cls_kde_plot("performance_data/GB_results_total.csv","Gradient Boosting","learning_rate",0,1,0.3,1.3)
+    cls_kde_plot("../performance_data/GB_results_total.csv","Gradient Boosting","learning_rate",0,1,0.3,1.3)
 
     # SVM_ sigmoid
-    cls_kde_plot("performance_data/SVM_results_total.csv","SVM","kernel",0.014,10,0,1.22,kernel="sigmoid",scale="log")
+    cls_kde_plot("../performance_data/SVM_results_total.csv","SVM","kernel",0.014,10,0,1.22,kernel="sigmoid",scale="log")
 
 
     # SVM_RBF
-    cls_kde_plot("performance_data/SVM_results_total.csv","SVM","kernel",0.01,10,0,0.14,kernel="rbf",scale="log")
+    cls_kde_plot("../performance_data/SVM_results_total.csv","SVM","kernel",0.01,10,0,0.14,kernel="rbf",scale="log")
